15df9bb09be368f23fdf78a78c81eaf5d5626bb1
Darryn J. Reinertson <35150825+darryn11@users.noreply.github.com>
2018-11-14 16:00:58 -0800

    Add files via upload

---
+++ home.js
@@ -0,0 +1,23 @@
+$('.triggerAbout').mouseover(function(){
+  $('.aboutOverlay').css('opacity', '0.7');
+}).mouseout(function(){
+  $('.aboutOverlay').css('opacity', '0');
+})
+
+$('.triggerLocation').mouseover(function(){
+  $('.locationOverlay').css('opacity', '0.7');
+}).mouseout(function(){
+  $('.locationOverlay').css('opacity', '0');
+})
+
+$('.triggerAccommodations').mouseover(function(){
+  $('.accommodationsOverlay').css('opacity', '0.7');
+}).mouseout(function(){
+  $('.accommodationsOverlay').css('opacity', '0');
+})
+
+$('.triggerAmenities').mouseover(function(){
+  $('.amenitiesOverlay').css('opacity', '0.7');
+}).mouseout(function(){
+  $('.amenitiesOverlay').css('opacity', '0');
+})
