c942d8cd38ff3608ea4477b6cff3cad4ddbb230f
Darryn J. Reinertson <35150825+darryn11@users.noreply.github.com>
2019-07-11 08:49:55 -0700

    Version 1.1
    Changing Location section to be more categorical instead of all at once. 
    Will also change the home accommodations section to be a slideshow instead of layered tiles.

--- home.js
+++ home.js
@@ -1,23 +1,52 @@
-$('.triggerAbout').mouseover(function(){
-  $('.aboutOverlay').css('opacity', '0.7');
-}).mouseout(function(){
-  $('.aboutOverlay').css('opacity', '0');
-})
+//AboutPage mouseover and mouseleave events.
+$('.aboutTrigger').mouseover(function(){
+  $('.aboutOverlay').css({
+    "opacity": "0.7"
+  });
+  $('.aboutTrigger').hide();
+});
+$('.aboutText').mouseleave(function(){
+  $('.aboutTrigger').show();
+  $('.aboutOverlay').css({
+    'opacity':'0'
+  });
+});
 
-$('.triggerLocation').mouseover(function(){
-  $('.locationOverlay').css('opacity', '0.7');
-}).mouseout(function(){
-  $('.locationOverlay').css('opacity', '0');
-})
+$('.locationTrigger').mouseover(function(){
+  $('.locationOverlay').css({
+    "opacity": "0.7"
+  });
+  $('.locationTrigger').hide();
+});
+$('.locationText').mouseleave(function(){
+  $('.locationTrigger').show();
+  $('.locationOverlay').css({
+    'opacity':'0'
+  });
+});
 
-$('.triggerAccommodations').mouseover(function(){
-  $('.accommodationsOverlay').css('opacity', '0.7');
-}).mouseout(function(){
-  $('.accommodationsOverlay').css('opacity', '0');
-})
+$('.accommodationsTrigger').mouseover(function(){
+  $('.accommodationsOverlay').css({
+    "opacity": "0.7"
+  });
+  $('.accommodationsTrigger').hide();
+});
+$('.accommodationsText').mouseleave(function(){
+  $('.accommodationsTrigger').show();
+  $('.accommodationsOverlay').css({
+    'opacity':'0'
+  });
+});
 
-$('.triggerAmenities').mouseover(function(){
-  $('.amenitiesOverlay').css('opacity', '0.7');
-}).mouseout(function(){
-  $('.amenitiesOverlay').css('opacity', '0');
-})
+$('.amenitiesTrigger').mouseover(function(){
+  $('.amenitiesOverlay').css({
+    "opacity": "0.7"
+  });
+  $('.amenitiesTrigger').hide();
+});
+$('.amenitiesText').mouseleave(function(){
+  $('.amenitiesTrigger').show();
+  $('.amenitiesOverlay').css({
+    'opacity':'0'
+  });
+});
